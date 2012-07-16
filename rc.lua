bee7e0a6c70bcf3dc61cb008aa3ac11d6ced9874
Lue Andrie-Her <lue.her@gmail.com>
2012-07-16 06:04:12 -0700

    Changed default layout.

--- rc.lua
+++ rc.lua
@@ -31,13 +31,13 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
     awful.layout.suit.spiral.dwindle,
     awful.layout.suit.spiral,
     awful.layout.suit.floating,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.max,
